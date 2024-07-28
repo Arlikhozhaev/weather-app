e9b96e39671b240817592a03ea00e4413d6bceb7
Jayden <jayxu231@gmail.com>
2024-07-27 21:28:20 -0700

    api gathers realtime data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -12,16 +12,47 @@ import humidity_icon from '../assets/humidity.png'
 
 const Weather = () => {
     
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon,
+
+
+    }
     const search = async (cityName)=>{
         try {
+            //for temp in celsius use &units=metric for temp in F &units imperial
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}
-            &appid=${import.meta.env.VITE_APP_ID}`;
+            &units=imperial&appid=${import.meta.env.VITE_APP_ID}`;
             // ${} used to insert things into sections
             const response = await fetch(url);
 
             const data = await response.json();
             console.log(data);
+            // we can use setter function to store data into weather data state
+            const icon = allIcons[data.weather[0].icon || clear_icon];
 
+            setWeatherData({
+                //key values for the app
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
 
         } catch (error) {
             
@@ -39,20 +70,20 @@ const Weather = () => {
             <img src={search_icon} alt="" />
         </div>
         <img src={clear_icon} alt="" className='weather-icon'/>
-        <p className='temperature'>16°C</p>
-        <p className='location'>Tokmok</p>
+        <p className='temperature'>{weatherData.temperature}°C</p>
+        <p className='location'>{weatherData.location}</p>
         <div className="weather-data">
             <div className="col">
                 <img src={humidity_icon} alt="" />
                 <div>
-                    <p>91 %</p>
+                    <p>{weatherData.humidity}%</p>
                     <span>Humidity</span>
                 </div>
             </div>
             <div className="col">
                 <img src={wind_icon} alt="" />
                 <div>
-                    <p>3.6 Km/h</p>
+                    <p>{weatherData.windSpeed} Km/h</p>
                     <span>Wind Speed</span>
                 </div>
             </div>
